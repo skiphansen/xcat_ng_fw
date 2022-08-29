75ec6ff2afe197865ba4d659a1023252f9900146
Skip Hansen <skip@gfrn.org>
2022-08-29 16:46:06 -0700

    Added rx/tx freqency support (only low band tested so far)

--- research/cp_dump.py
+++ research/cp_dump.py
@@ -25,6 +25,78 @@ import argparse
 
 log = False
 
+RangeTbl = (
+    ('29.7 - 50.0',-75.7e6),
+    ('136 - 154',-53.9e6),
+    ('148 - 172',-53.9e6),
+    ('148 - 174',-53.9e6),
+    ('150 - 174',-53.9e6),
+    ('406 - 420',-53.9e6),
+    ('420 - 430',-53.9e6),
+    ('450 - 470',53.9e6),
+    ('470 - 488',53.9e6),
+    ('482 - 500',53.9e6),
+    ('494 - 512',53.9e6),
+    ('806 - 870',53.9e6)
+)
+
+RefTbl = (('not used',0),('6.25 Khz',6250),('4.16667 Khz',4167),('5 Khz',5000))
+
+CTbl = (-1,1,0,2)
+LowBand = False
+Mhz800 = False
+IfFreq = 0
+
+#f(vco) = ((((64 * A) + (63 * B)) * 3 ) + C(table)) * f(ref)
+#low band: highside injection, add IF frequency of 75.7 Mhz
+#VHF: highside injection, add IF frequency of 53.9 Mhz
+#UHF: lowside injection, subtract IF frequency of 53.9 Mhz
+def calc_fco(synth_bytes):
+    if log:
+        print(f'Raw bytes 0x{synth_bytes[0]:02x} 0x{synth_bytes[1]:02x} 0x{synth_bytes[2]:02x}')
+
+    A = synth_bytes[0] & 0x3f
+    B = ((synth_bytes[0] & 0xc0) >> 6) + (synth_bytes[1] << 2)
+
+    R = synth_bytes[2] & 0x3
+    V = (synth_bytes[2] & 0xc) >> 2
+    C = (synth_bytes[2] & 0x30) >> 4
+    S = (synth_bytes[2] & 0xc0) >> 6
+    if log:
+        print(f'A {A} 0x{A:02x}, B {B} 0x{B:02x}, V {V}, R {R}, C {C}')
+    fref = RefTbl[R][1]
+    if log:
+        print(f'fref {fref}')
+    fvco = (64 * A) + (63 * B)
+    if not LowBand:
+        fvco *= 3
+        #c bits are extender control for lowband C1 = 1 C0 = extender on/off
+        c = CTbl[C]
+        if log:
+            print(f'c {c}')
+        fvco += c
+    fvco *= fref
+    if log:
+        print(f'fvco {fvco/1e6}')
+    return fvco
+
+def calc_rx(fvco):
+    if fvco == 0:
+        return fvco
+    else:
+        return fvco + IfFreq
+
+def calc_tx(fvco):
+    if fvco == 0:
+        return fvco
+    elif LowBand:
+        return 172.8e6- fvco
+    elif Mhz800:
+        return fvco * 2
+    else:
+        return fvco
+
+
 args = len(sys.argv)
 
 parser = argparse.ArgumentParser()
@@ -68,35 +140,43 @@ if args.File:
         else:
             print('Checksum is valid')
 
+        print(f'SB9600 address 0x{plug[7]:02x}')
         modes = plug[8]
         if modes == 0:
-            print(f'Code plug defines no modes?!?')
-            exit(0)
+            print(f'No modes defined ?!?')
+            break
 
-        print(f'SB9600 address 0x{plug[7]:02x}')
         s=''
         if modes > 1:
             s='s'
         print(f'{plug[8]} mode{s}:')
-        print(f'Mode table has {plug[9]} bytes entries')
+        ModeLen = plug[9]
+        print(f'Mode table has {ModeLen} bytes entries')
 
-        #f(vco) = ((((64 * A) + (63 * B)) * 3 ) + C(table)) * f(ref)
-        #low band: highside injection, add IF frequency of 75.7 Mhz
-        #VHF: highside injection, add IF frequency of 53.9 Mhz
-        #UHF: lowside injection, subtract IF frequency of 53.9 Mhz
+        RadioRange = plug[0x2001 - 1]
+        if RadioRange < 1 or RadioRange > 0xc:
+            print(f'Invalid radio range 0x{RadioRange:02x} ?!?')
+            break
+        print(f'Radio range {RangeTbl[RadioRange-1][0]} Mhz')
+        IfFreq = RangeTbl[RadioRange-1][1]
+        LowBand = (RadioRange == 1)
+        Mhz800 = (RadioRange == 0xc)
 
-        #N = fvco / refreq
-        #c = N % 3; N1 = N / 3;
-
-        # assume low band for now
-
-        ref_freq = 0
-        #frx = fvco - 75700000;
-        #fvco = ((64 * txa) + (63 * txb)) * refreq;
-        #ftx = 172800000 - fvco;
-
-
+        mode = 1
+        while mode <= modes:
+            i = 0x100 + ((mode - 1) * ModeLen)
+            fvco = calc_fco((plug[i],plug[i + 1],plug[i+2]))
+            frx = calc_rx(fvco)
+            fvco = calc_fco((plug[i+3],plug[i + 4],plug[i+5]))
+            ftx = calc_tx(fvco)
+            if ftx != 0 or frx != 0:
+                print(f'Mode {mode}:')
+                if frx != 0:
+                    print(f'frx {frx/1e6}')
+                if ftx != 0:
+                    print(f'ftx {ftx/1e6}')
+                print('')
+            mode += 1
 
     fp.close()
-
                 
